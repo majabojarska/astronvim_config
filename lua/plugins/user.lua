95e6df2941ae5a1a5392bdd81860b8af28413e6a
Maja Bojarska <majabojarska98@gmail.com>
2025-10-17 16:27:18 +0200

    First commit

---
+++ lua/plugins/user.lua
@@ -0,0 +1,11 @@
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
+-- Here are some examples:
+
+---@type LazySpec
+return {
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" },
+  },
+}
