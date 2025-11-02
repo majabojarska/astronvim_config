8556adb2f1129159e0544855218fb9917de4428d
Maja Bojarska <majabojarska98@gmail.com>
2025-11-02 15:46:29 +0100

    add many astrocommunity plugins

--- lua/community.lua
+++ lua/community.lua
@@ -5,13 +5,32 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- https://astronvim.github.io/astrocommunity/
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
-  -- import/override with your plugins folder
-
-  { import = "astrocommunity.colorscheme.gruvbox-baby" },
   { import = "astrocommunity.pack.rust" },
+
+  -- Editor
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  { import = "astrocommunity.recipes.ai" },
+
+  -- Workflow
+  { import = "astrocommunity.workflow.bad-practices-nvim" },
+
+  -- Docker
+  { import = "astrocommunity.docker.lazydocker" },
+
+  -- Git
+  { import = "astrocommunity.git.gitlinker-nvim" },
+
+  -- MD, try out and choose one later
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- Themes
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
 }
